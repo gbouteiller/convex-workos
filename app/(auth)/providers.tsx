a817768c1fcd9e8638aeba5d44da5718502f5916
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2025-08-29 12:21:07 +0400

    Update providers.tsx

--- app/(auth)/providers.tsx
+++ app/(auth)/providers.tsx
@@ -5,7 +5,7 @@ import { ConvexProviderWithAuth, ConvexReactClient } from "convex/react";
 import { type PropsWithChildren, useCallback, useRef } from "react";
 
 if (!process.env.NEXT_PUBLIC_CONVEX_URL) throw new Error("Missing env var");
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL, { expectAuth: true });
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL, { expectAuth: true, verbose: true });
 
 export function Providers({ children }: PropsWithChildren) {
 	return (
